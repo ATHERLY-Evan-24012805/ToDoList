900a9ae740c6f705c7cb5c59247fdf56445754a5
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2026-03-20 12:01:43 +0100

    ajout des notes qui s'affiche quand le dossier est cliqué

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 import logo from './img/logo.svg';
 import './style/App.css';
 import data from './data/data.json';
+import { useState } from 'react';
+
+
+function getTasksByFolder(folderId){
+  const LItems = data.relations.filter(relation => relation.categorie === folderId);
+  const idsDesTaches = LItems.map(relation => relation.tache);
+  const LTask = data.tasks.filter(task => idsDesTaches.includes(task.id));
+  return LTask;
+}
+
+function DisplayContent({ folderId }){
+  console.log("laaaaaaaaaaaaaaaaaaa")
+
+  if (folderId === null){
+    return null;
+  }
+
+  const LTask = getTasksByFolder(folderId)
+
+  return (
+    <div class = "text">
+      {LTask.map((task)=> (
+        <div class="item" key={task.id} style ={{backgroundColor:"#808080"}}>
+          <h4>{task.title}</h4>
+          <p>description : {task.description}</p>
+          <p>date de creation : {task.date_creation}</p>
+          <p>date d'echéance : {task.date_echeance}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 
 function App (){
+  const [selectedFolderId, setSelectedFolderId] = useState(null);
   return (
     <div class="folderArea">
       <div class="list">
         <h1>Vos catégories:</h1>
-        {data.categories.map((categories)=>
-          <div class="item" key = {categories.id}
-          style={{backgroundColor:categories.color}}>
-            <h3>{categories.title}</h3>
-            <img src={categories.icon}></img>
+        {data.categories.map((categorie)=>
+          <div class="item" key = {categorie.id}
+          style={{backgroundColor:categorie.color}} onClick={() => setSelectedFolderId(categorie.id)}>
+            <h3>{categorie.title}</h3>
+            <img src={categorie.icon}></img>
           </div>
-        )}
-        
+        )}   
       </div>
-    <div class="text"></div>
+      <DisplayContent folderId={selectedFolderId} />
     </div>
-    
   )
 }
 export default App;
-
-
-
-
 
 
   {/* const folderArea = document.createElement("div")
